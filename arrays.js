d7eb5fda672081d2ee5ccc1c07e1e302869f56f5
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-09-07 17:17:51 -0300

    principais funcao do arrya

--- arrays.js
+++ arrays.js
@@ -48,3 +48,65 @@ ordenacaoArray.forEach(n => { console.log(n); });
 // se precisar alterar um array, pode utilizar o map
 // nesse caso irá multiplicar tudo por 2
 ordenacaoArray.map(n => n * 2);
+
+
+// quebrando um string com joi separadar
+let arrayComSeparador = [1,2,3,4,5,5,6,7,8,9,];
+arrayComSeparador.join('-');
+// resultado "1-2-3-4-5-5-6-7-8-9"
+
+
+
+// a funcao join pode ser usada com o split
+// que irá transformar uma string em array e depois colocar o separador
+"abc".split('').join('-')
+
+// resultado "a-b-c"
+
+// com a funcao reverse é possivel inverter o array
+[1,2,3].reverse
+
+//resultado [3, 2, 1]
+
+
+//inverter um string
+'abc'.split('').reverse().join('-');
+// resultado "c-b-a"
+
+// inserir um novo elemento
+myArray.push('asda');
+
+// remover um elemento
+myArray.pop();
+
+// adicionar um elemente no inicio do array
+myArray.unshift("asDAS");
+
+// remover a primeira posicao do array
+myArray.shift();
+
+
+
+//removendo um elemente pelo indice
+// primeiro parametro do () é o index depois a quantidade de elementos a ser removido
+let removendoElementos = [1,2,3,4,5,6,7,8,9];
+// removendo o elemento 4
+removendoElementos.splice(3,1);
+// resultado [4]
+// removendoElementos
+// (8) [1, 2, 3, 5, 6, 7, 8, 9]
+
+// se nao passar nada no segundo parametro do slice e
+// um valor como terceiro paramentro irá alterar naquela posicao pelo novo valor informado
+removendoElementos.splice(2, 0, 'alicio');
+
+//resultado removendoElementos
+// (10) [1, 2, "alicio", 3, 4, 5, 6, 7, 8, 9]
+
+// pegando uma substrign  iniciando com o index
+// e vai até o valor informado 
+arrayComSeparador.slice(1, 4);
+
+//  irá pegar todos os elemente a partir 
+// do index 1 e vai pegar os proximos 3 index
+//resultado  [2, 3, 4]
